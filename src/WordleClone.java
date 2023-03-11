f1345f68b64c21d2fd058c49d7bbb3356e611666
Darren Greaux <greauxd@gmail.com>
2023-03-11 12:53:10 -0500

    Converted program to use a GUI using JavaFX

--- src/WordleClone.java
+++ src/WordleClone.java
@@ -1,80 +1,135 @@
 import java.io.*;
-import java.io.RandomAccessFile;
 import java.util.Random;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.control.Button;
 
-public class WordleClone {
 
-    public static void main(String args[]) {
 
-        // Initializes variable guess
-        String guess = "";
+public class WordleClone extends Application {
+
+    @Override
+
+    public void start(Stage primaryStage) {
+
+        //Create an array of TextFields
+        TextField[] letters = new TextField[5];
+
+        //Declare hBox for TextField and vBox for the opening message
+        HBox hBox = new HBox();
+        VBox vBox = new VBox();
+
+        //Create padding for hBox and vBox
+        hBox.setPadding(new Insets(100, 100, 100, 200));
+        vBox.setPadding(new Insets(10, 100, 10, 150));
+
+        //Spacing for TextField boxes
+        hBox.setSpacing(5);
+
+        //Creates 5 TextField boxes to fill array, sets sizes/fonts for each TextField box
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] = new TextField();
+            letters[i].setPrefColumnCount(1);
+            letters[i].setMinSize(50, 50);
+            letters[i].setFont(Font.font("Verdana", FontWeight.BOLD, 18));
+            hBox.getChildren().addAll(letters[i]);
+        }
+
+        //Declare BorderPane
+        BorderPane pane = new BorderPane();
+
+        //Creates text for introduction and description messages
+        Text introduction = new Text(50, -150, "Welcome to my Wordle Clone!");
+        Text description = new Text(30, -100, "The goal of the game is to guess the correct word, \nyou will have 6 attempts to guess, whether you win or \nlose you get to try again with a new word! \n\n                                 GOOD LUCK!!!");
+
+        //Changes font for introduction and description
+        introduction.setFont(Font.font("Verdana", 20));
+        description.setFont(Font.font("Verdana", 12));
+
+        vBox.getChildren().addAll(introduction, description);
+
+        //Initialize variable guesses
+        int guesses = 0;
+
+        //Fills pane with vBox and hBox, vBox, which contains introduction and description is at the top, and the hBox, which has the TextFields is in the center
+        pane.setCenter(hBox);
+        pane.setTop(vBox);
 
         // Calls function getAnswer to get random word from text file
         String answer = getAnswer();
-        // Assigns int guesses to 0
-        int guesses = 0;
 
-        // Declares variable input to take in user input
-        Scanner input = new Scanner(System.in);
+        System.out.println(answer);
 
-        // Loop that will iterate until user makes 6 guesses, or if user guesses answer correctly
-        do {
 
-            // Asks and takes input so long as input is 5 characters long
-            do {
-                System.out.print("Guess: ");
-                guess = input.nextLine();
-                if(guess.length() != 5){
-                    System.out.println("Guess must be 5 characters");
-                }
-                checkGuess(guess);
-            } while(guess.length() != 5);
+        //Creates scene and sets it
+        Scene scene = new Scene(pane, 650, 400);
+        primaryStage.setTitle("WordleClone");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
-            // For loop that goes through each character in guess to compare to characters in answer
-            for(int i = 0; i < guess.length(); i++) {
 
-                // Compares ith character of guess to ith character in answer, and will print a 1 if it is a match, else it will print a 0 if there is no match
-                if(guess.charAt(i) == answer.charAt(i)) {
-                    System.out.print("1");
-                }
-                else if(answer.indexOf(guess.charAt(i)) != -1) {
-                    System.out.print("2");
-                }
-                else {
-                    System.out.print("0");
-                }
+        letters[0].setOnKeyReleased(e -> {
+            if(letters[0].getText().length() >= 1){
+                letters[1].requestFocus();
             }
-
-            // Prints line after showing how many characters user guessed correctly
-            System.out.println();
-
-            // Iterates guesses by 1
-            guesses += 1;
-
-            // Displays correct answers when user runs out of guesses or user guesses the correct answer, and prompts users if they want to retry
-            if(guesses == 6 && guess.compareTo(answer) != 0 || guess.compareTo(answer) ==0){
-                System.out.println("Correct answer is " + answer);
-                if(guess.compareTo(answer) == 0) {
-                    System.out.println("CONGRATS!!!");
-                }
-                System.out.print("Want to try again: ");
-                String retry = input.nextLine();
-                retry = retry.toLowerCase();
-                if(retry.equals("y") || retry.equals("yes")) {
-                    answer = getAnswer();
-                    guesses = 0;
-                }
+            if(e.getCode() == KeyCode.ENTER){
+                letters[0].requestFocus();
             }
+        });
+
+        letters[1].setOnKeyReleased(e -> {
+            if(letters[1].getText().length() >= 1){
+                letters[2].requestFocus();
+            }
+        });
+
+        letters[2].setOnKeyReleased(e -> {
+            if(letters[2].getText().length() >= 1){
+                letters[3].requestFocus();
+            }
+        });
+
+        letters[3].setOnKeyReleased(e -> {
+            if(letters[3].getText().length() >= 1){
+                letters[4].requestFocus();
+            }
+        });
 
 
-        } while(guesses < 6 && guess.compareTo(answer) != 0);
+            scene.setOnKeyPressed(e -> {
+                //Initializes variable guess
+                String guess = "";
 
+                if(e.getCode() == KeyCode.ENTER) {
+                    //Concatenates first letter of TextField to guess
+                    for (int i = 0; i < 5; i++)
+                        guess = guess.concat(letters[i].getText(0, 1));
+                }
+                    //Calls checkGuess function
+                    checkGuess(guess, letters, answer, pane, incrementGuesses(guesses));
+                    letters[0].requestFocus();
+            });
 
-    }
-
+            }
 
     private static String getAnswer() {
+
+        //Function that will return a random word from dictionary txt file, and assigns it to variable answer
+
         try {
             BufferedReader file = new BufferedReader(new FileReader("E:/IntelliJ/WordleClone/src/dictionary.txt"));
 
@@ -92,29 +147,43 @@ public class WordleClone {
                 index++;
             }
         }
+        //Exception thrown when file cannot be found
         catch(Exception noFile){
             System.out.println("That file cannot be found");
         }
         return("Answer cannot be found");
     }
 
-    private static String checkGuess(String guess) {
-        try {
-            BufferedReader file = new BufferedReader(new FileReader("E:/IntelliJ/WordleClone/src/dictionary.txt"));
+    private static String checkGuess(String guess, TextField[] letters, String answer, BorderPane pane, int guesses) {
+        //Function that will compare guess to answer
 
-            int index = 1;
+        HBox guessesBox = new HBox();
 
-            while ((file.readLine()) != null) {
-                if (guess.equals(index)) {
+        System.out.println(guess);
+        for (int i = 0; i < guess.length(); i++) {
 
-                    return guess;
-                }
-                index++;
+            //Compares ith character of guess to ith character in answer, will change TextField to green and sets editable to false if letter is in the correct place in the word,
+            //yellow if the letter is in the word, but not in the right place and red if the letter is not in the word at all
+            if (guess.charAt(i) == answer.charAt(i)) {
+                letters[i].setStyle("-fx-background-color: green");
+                letters[i].setEditable(false);
+            } else if (answer.indexOf(guess.charAt(i)) != -1) {
+                letters[i].setStyle("-fx-background-color: gray");
+            } else {
+                letters[i].setStyle("-fx-background-color: red");
             }
         }
-        catch(Exception noFile){
-            System.out.println("That file cannot be found");
-        }
-        return("Guessed word is not found in dictionary");
+        Text guessText = new Text("Guesses: " + guesses);
+        guessesBox.getChildren().add(guessText);
+
+        pane.setBottom(guessesBox);
+
+        return guess;
+
     }
+    private int incrementGuesses(int guesses){
+        guesses += 1;
+        return guesses;
+    }
+
 }
